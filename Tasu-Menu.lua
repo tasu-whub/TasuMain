f93ef6d9548431491f6aaaba0dbfe536df99b97e
Assut <tasuhub@gmail.com>
2026-09-20 11:51:44 +0700

    Update Tasu-Menu.lua

--- Tasu-Menu.lua
+++ Tasu-Menu.lua
@@ -7,4 +7,4 @@
    ╚═╝       ╚═╝  ╚═╝    ╚═════╝       ╚═════╝
 -- buy Tasu Hub V3.1 in my discord | https://tasu-community.tasuhub.workers.dev/
 ]]
-loadstring(game:HttpGet("https://pastefy.app/YkNbpfQC/raw"))()
+loadstring(game:HttpGet("https://xhider.xyz/raw/TasuHubBF/Menu.lua"))()
